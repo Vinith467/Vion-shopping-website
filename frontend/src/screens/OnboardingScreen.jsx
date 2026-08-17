e61a7d0b90dd1b5039e40f4a63ebc13e36f18c38
VINITH S SHETTY <vinuvinith0007@gmail.com>
2026-08-17 17:36:33 +0530

    fix: move sticky action bar above mobile bottom navigation

--- frontend/src/screens/OnboardingScreen.jsx
+++ frontend/src/screens/OnboardingScreen.jsx
@@ -1223,7 +1223,7 @@ export default function OnboardingScreen() {
                   </div>
 
                   {/* Actions */}
-                  <div className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-100 p-4 flex gap-3 shadow-[0_-4px_20px_rgba(0,0,0,0.05)] lg:static lg:p-0 lg:border-none lg:shadow-none lg:bg-transparent lg:pt-2">
+                  <div className="fixed bottom-[72px] left-0 right-0 z-50 bg-white border-t border-gray-100 p-4 flex gap-3 shadow-[0_-4px_20px_rgba(0,0,0,0.05)] lg:static lg:p-0 lg:border-none lg:shadow-none lg:bg-transparent lg:pt-2">
                     <button 
                       onClick={() => {
                         const numericPrice = selectedProduct.price.replace(/[^0-9.]/g, '');
